040ed4407699f4b5bf2734a85409d3ef5b543be1
asano69 <186772504+asano69@users.noreply.github.com>
2026-04-29 12:35:57 +0900

    画像の保存場所変更中

--- internal/handler/item.go
+++ internal/handler/item.go
@@ -20,7 +20,7 @@ func newItem(db *sql.DB) http.HandlerFunc {
 		renderTemplate(w, "item_new.html", nil)
 	}
 }
-func createItem(db *sql.DB) http.HandlerFunc {
+func createItem(db *sql.DB, dataDir string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if err := r.ParseMultipartForm(maxUploadSize); err != nil {
 			http.Error(w, "file too large", http.StatusBadRequest)
@@ -46,7 +46,7 @@ func createItem(db *sql.DB) http.HandlerFunc {
 			defer file.Close()
 
 			filename := fmt.Sprintf("%d_%s", time.Now().UnixNano(), filepath.Base(header.Filename))
-			dst := filepath.Join("data", "img", filename)
+			dst := filepath.Join(dataDir, "img", filename)
 
 			if err := os.MkdirAll(filepath.Dir(dst), 0755); err != nil {
 				http.Error(w, "failed to create upload directory", http.StatusInternalServerError)
@@ -65,7 +65,7 @@ func createItem(db *sql.DB) http.HandlerFunc {
 				return
 			}
 
-			item.Image = "/data/img/" + filename
+			item.Image = "/img/" + filename
 		}
 
 		if err := store.Save(db, item); err != nil {
--- internal/handler/routes.go
+++ internal/handler/routes.go
@@ -5,9 +5,9 @@ import (
 	"net/http"
 )
 
-func RegisterRoutes(mux *http.ServeMux, db *sql.DB) {
+func RegisterRoutes(mux *http.ServeMux, db *sql.DB, dataDir string) {
 	mux.HandleFunc("GET /{$}", index)
 	mux.Handle("GET /static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 	mux.HandleFunc("GET /items/new", newItem(db))
-	mux.HandleFunc("POST /items/new", createItem(db))
+	mux.HandleFunc("POST /items/new", createItem(db, dataDir))
 }
