fbaa531344221ad165df15dc1fe9db55524b4917
asano69 <186772504+asano69@users.noreply.github.com>
2026-04-29 12:42:27 +0900

    画像の保存場所変更完了

--- internal/handler/routes.go
+++ internal/handler/routes.go
@@ -3,11 +3,14 @@ package handler
 import (
 	"database/sql"
 	"net/http"
+	"path/filepath"
 )
 
+// internal/handler/routes.go
 func RegisterRoutes(mux *http.ServeMux, db *sql.DB, dataDir string) {
 	mux.HandleFunc("GET /{$}", index)
 	mux.Handle("GET /static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	mux.Handle("GET /img/", http.StripPrefix("/img/", http.FileServer(http.Dir(filepath.Join(dataDir, "img")))))
 	mux.HandleFunc("GET /items/new", newItem(db))
 	mux.HandleFunc("POST /items/new", createItem(db, dataDir))
 }
